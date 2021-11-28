7d8d295d7ed3cb6a86664994b53e23642a4d1cc7
kenzostore <90239401+kenzostore@users.noreply.github.com>
2021-11-28 21:19:21 +0700

    Update allpayment.js
    ...

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -1,27 +1,27 @@
 const allpayment = (prefix) => {
-return `PAYMENT BY ZEEONE OFC
+return `PAYMENT BY KENZ FC
 
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ Ke Owner Aja
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ Ke Owner Aja
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ Ke Owner Aja
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ Ke Owner Aja
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ Ke Owner Aja
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ Ke Owner Aja
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ Ke Owner Aja
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama Di Owner Ja
 
 *📲 PULSA*
 
