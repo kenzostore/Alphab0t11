bf2f4cb44c161b0f6cd31dcdf3998ea8c85d738d
kenzostore <90239401+kenzostore@users.noreply.github.com>
2021-11-29 08:42:48 +0700

    Update donasi.js
    Dona

--- shop/donasi.js
+++ shop/donasi.js
@@ -4,24 +4,24 @@ return `DONASI BISA VIA BANK / E-MONEY
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ Owner
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ Owner
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ Owner
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ Owner
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ Owner
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ Owner
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ Owner
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama KenzStore
 `
 	}
 
